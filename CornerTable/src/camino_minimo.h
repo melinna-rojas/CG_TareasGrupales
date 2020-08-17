e56c0123e26eea412bc0860a27612c73f453fd57
Melinna <melin1504@gmail.com>
2020-08-17 00:33:58 -0500

    main

--- CornerTable/src/camino_minimo.h
+++ CornerTable/src/camino_minimo.h
@@ -5,24 +5,29 @@
 #include <vector>
 #include <set>
 using namespace std;
-struct Triangle{
+struct Triangle
+{
     unsigned int vertex1, vertex2, vertex3;
 };
 
 #define INF 10000
 
-int minCost(vector<unsigned int> cost, vector<bool> marked){
+int minCost(vector<unsigned int> cost, vector<bool> marked)
+{
     int min = INF;
-	  int i_min = -1;
-    for (unsigned int i=0; i<cost.size(); i++){
-        if (!marked[i] && cost[i]<min){
+    int i_min = -1;
+    for (unsigned int i = 0; i < cost.size(); i++)
+    {
+        if (!marked[i] && cost[i] < min)
+        {
             min = cost[i];
             i_min = i;
         }
     }
     return i_min;
 }
-void DijkstraPath(vector<unsigned int> &path, vector<vector<unsigned int>> adjmat, int i, int d){
+void DijkstraPath(vector<unsigned int> &path, vector<vector<unsigned int>> adjmat, int i, int d)
+{
     vector<bool> marked(adjmat.size(), 0);
     vector<unsigned int> cost(adjmat.size(), INF);
     path.resize(adjmat.size());
@@ -30,47 +35,57 @@ void DijkstraPath(vector<unsigned int> &path, vector<vector<unsigned int>> adjma
 
     cost[i] = 0;
 
-    for (unsigned int i=0; i<adjmat.size(); i++){
-		   int i_node = minCost(cost, marked);
+    for (unsigned int i = 0; i < adjmat.size(); i++)
+    {
+        int i_node = minCost(cost, marked);
         if (i_node == -1) // all is marked
             break;
         marked[i_node] = true;
-        for (unsigned int j=0; j<adjmat.size(); j++){
-            if (marked[j] || !adjmat[i_node][j]) continue;
-            if (cost[i_node] + adjmat[i_node][j] < cost[j]){
-                cost[j] =  cost[i_node] + adjmat[i_node][j];
+        for (unsigned int j = 0; j < adjmat.size(); j++)
+        {
+            if (marked[j] || !adjmat[i_node][j])
+                continue;
+            if (cost[i_node] + adjmat[i_node][j] < cost[j])
+            {
+                cost[j] = cost[i_node] + adjmat[i_node][j];
                 path[j] = i_node;
             }
         }
     }
 }
-bool isConected(Triangle A, Triangle B){
+bool isConected(Triangle A, Triangle B)
+{
     set<unsigned int> sa;
     sa.insert(A.vertex1);
     sa.insert(A.vertex2);
     sa.insert(A.vertex3);
     int cont = 0;
-    if (sa.find(B.vertex1) != sa.end()) cont++;
-    if (sa.find(B.vertex2) != sa.end()) cont++;
-    if (sa.find(B.vertex3) != sa.end()) cont++;
-    return cont>1;
+    if (sa.find(B.vertex1) != sa.end())
+        cont++;
+    if (sa.find(B.vertex2) != sa.end())
+        cont++;
+    if (sa.find(B.vertex3) != sa.end())
+        cont++;
+    return cont > 1;
 }
-void buildAdjMat(CornerTable* CT, vector<vector<unsigned int>> &adjMat){
-    const CornerType* triangleList = CT->getTriangleList();
+void buildAdjMat(CornerTable *CT, vector<vector<unsigned int>> &adjMat)
+{
+    const CornerType *triangleList = CT->getTriangleList();
     unsigned int numTriangles = CT->getNumTriangles();
     adjMat.resize(numTriangles);
     fill(adjMat.begin(), adjMat.end(), vector<unsigned int>(numTriangles, 0));
-    for (unsigned int i=0; i<numTriangles; i++){
-        Triangle v{triangleList[3*i],
-                triangleList[3*i + 1],
-                triangleList[3*i + 2]
-                };
-        for (unsigned int j=i+1; j<numTriangles; j++){
-            Triangle comp{triangleList[3*j],
-                        triangleList[3*j + 1],
-                        triangleList[3*j + 2]
-                        };
-            if (isConected(v, comp)){
+    for (unsigned int i = 0; i < numTriangles; i++)
+    {
+        Triangle v{triangleList[3 * i],
+                   triangleList[3 * i + 1],
+                   triangleList[3 * i + 2]};
+        for (unsigned int j = i + 1; j < numTriangles; j++)
+        {
+            Triangle comp{triangleList[3 * j],
+                          triangleList[3 * j + 1],
+                          triangleList[3 * j + 2]};
+            if (isConected(v, comp))
+            {
                 adjMat[i][j] = 1;
                 adjMat[j][i] = 1;
             }
@@ -78,29 +93,35 @@ void buildAdjMat(CornerTable* CT, vector<vector<unsigned int>> &adjMat){
     }
 }
 
-void mostrarCaminoDijkstra(vector<unsigned int> path, int o, int d){
-    cout<<"Camino: "<<endl;
+void mostrarCaminoDijkstra(vector<unsigned int> path, int o, int d)
+{
+    cout << "Camino: " << endl;
     int r = path[d];
-    cout<<d<<" <- ";
-    while(r > -1 && r != o){
-        cout<<r<<" <- ";
+    cout << d << " <- ";
+    while (r > -1 && r != o)
+    {
+        cout << r << " <- ";
         r = path[r];
     }
-    cout<<o<<endl;
+    cout << o << endl;
 }
-void min_path(vector<unsigned int> &path, CornerTable *CT, int o, int d) {
+void min_path(vector<unsigned int> &path, CornerTable *CT, int o, int d)
+{
 
-    vector<vector<unsigned int>> adjmat; buildAdjMat(CT, adjmat);
-	//Matriz adyacencia
-    if (CT->getNumTriangles() < 20){
-        for(unsigned int i =0; i<adjmat.size(); i++){
-            for (unsigned int j=0; j<adjmat.size(); j++){
-                cout<<adjmat[i][j]<<" ";
+    vector<vector<unsigned int>> adjmat;
+    buildAdjMat(CT, adjmat);
+    //Matriz adyacencia
+    if (CT->getNumTriangles() < 20)
+    {
+        for (unsigned int i = 0; i < adjmat.size(); i++)
+        {
+            for (unsigned int j = 0; j < adjmat.size(); j++)
+            {
+                cout << adjmat[i][j] << " ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
 
     DijkstraPath(path, adjmat, o, d);
-
 }
--- CornerTable/src/main.cpp
+++ CornerTable/src/main.cpp
@@ -8,19 +8,15 @@
 
 #include <GL/glew.h>
 
-
 // Incluir GLFW
 #include <GLFW/glfw3.h>
-
 
 // Incluir recursos creados
 #include "Utils.h"
 #include "CornerTable.h"
 
-
 #include "DefinitionTypes.h"
 #include "camino_minimo.h"
-
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -39,110 +35,120 @@ GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 GLuint ebo[numEBOs];
 
-
 int cantidadPuntos;
-double* vertexList1;
+double *vertexList1;
 int points;
 CornerTable *ct;
 vector<unsigned int> indiceCamino;
 
-void generadorPuntos(double &x1, double &y1,  double &x2 ,double &y2){
-    srand( time( NULL ) );
+void generadorPuntos(double &x1, double &y1, double &x2, double &y2)
+{
+    srand(time(NULL));
 
-    x1 = -3.0000 + (rand( ) % 60010 ) / 10000.0f;
-    y1= -3.0000 + (rand( ) % 60010 ) / 10000.0f;
-    x2 = -3.0000 + (rand( ) % 60010 ) / 10000.0f;
-    y2= -3.0000 + (rand( ) % 60010 ) / 10000.0f;
-
+    x1 = -3.0000 + (rand() % 60010) / 10000.0f;
+    y1 = -3.0000 + (rand() % 60010) / 10000.0f;
+    x2 = -3.0000 + (rand() % 60010) / 10000.0f;
+    y2 = -3.0000 + (rand() % 60010) / 10000.0f;
 }
 
-void ubicarCoordenada(double vertexList[],int npoint,double punto1x, double punto1y,int &punto){
-	double tempx,tempy;
-	double cercanox,cercanoy;
-	double cercanoxComparador,cercanoyComparador;
-	cercanoxComparador = 1000;
-	cercanoyComparador = 1000;
+void ubicarCoordenada(double vertexList[], int npoint, double punto1x, double punto1y, int &punto)
+{
+    double tempx, tempy;
+    double cercanox, cercanoy;
+    double cercanoxComparador, cercanoyComparador;
+    cercanoxComparador = 1000;
+    cercanoyComparador = 1000;
 
-	for(int i = 1 ; i <= npoint ;i++){
-		tempx = vertexList[(i-1)*3];
-		tempy = vertexList[(i-1)*3+1];
-		cercanox = tempx - punto1x;
-		if(cercanox<=0){
-			cercanox = cercanox*(-1.0);
-		}
-		cercanoy = tempy - punto1y;
-		if(cercanoy<=0){
-			cercanoy = cercanoy*(-1.0);
-		}
-		if(((punto1x - tempx) == 0) && ((punto1y - tempy) == 0)){
-			punto = i-1;
-			break;
-		}else{
-			if(cercanox<cercanoxComparador && cercanoy < cercanoyComparador){
-				cercanoxComparador = cercanox;
-				cercanoyComparador = cercanoy;
-				punto = i-1;
-			}
-		}
-	}
+    for (int i = 1; i <= npoint; i++)
+    {
+        tempx = vertexList[(i - 1) * 3];
+        tempy = vertexList[(i - 1) * 3 + 1];
+        cercanox = tempx - punto1x;
+        if (cercanox <= 0)
+        {
+            cercanox = cercanox * (-1.0);
+        }
+        cercanoy = tempy - punto1y;
+        if (cercanoy <= 0)
+        {
+            cercanoy = cercanoy * (-1.0);
+        }
+        if (((punto1x - tempx) == 0) && ((punto1y - tempy) == 0))
+        {
+            punto = i - 1;
+            break;
+        }
+        else
+        {
+            if (cercanox < cercanoxComparador && cercanoy < cercanoyComparador)
+            {
+                cercanoxComparador = cercanox;
+                cercanoyComparador = cercanoy;
+                punto = i - 1;
+            }
+        }
+    }
 }
 
-void leerMesh(){
-	ifstream fin;
-	fin.open("mesh/mesh21.mesh");
-	string name;
-	int npoint,ntriangle,var3;//Numero de puntos, orden y colores
-	int var0;;
-	fin>>name;
-	fin>>npoint>>ntriangle>>var3;
+void leerMesh()
+{
+    ifstream fin;
+    fin.open("mesh/mesh21.mesh");
+    string name;
+    int npoint, ntriangle, var3; //Numero de puntos, orden y colores
+    int var0;
+    ;
+    fin >> name;
+    fin >> npoint >> ntriangle >> var3;
 
-
-	double* vertexList = new double[3*npoint];
-	for(int i = 0; i<npoint;++i){
-		fin>>vertexList[3*i]>>vertexList[3*i+1]>>vertexList[3*i+2];
-	}
-	const CornerType numberTriangles = ntriangle;
-	const CornerType numberVertices = npoint;
-	CornerType* triangleList = new CornerType[3*ntriangle];
-	for(int i = 0; i<ntriangle;++i){
-		fin>>var0>>triangleList[i*3]>>triangleList[i*3+1]>>triangleList[i*3+2];
-	}
-	int numberCoordinatesByVertex = 3;
-	CornerTable *CT = new CornerTable(triangleList,
-									vertexList,
-									numberTriangles,
-									numberVertices,
-									numberCoordinatesByVertex
-	);
-	ct = CT;
-	cantidadPuntos = npoint;
-	vertexList1= vertexList;
-
+    double *vertexList = new double[3 * npoint];
+    for (int i = 0; i < npoint; ++i)
+    {
+        fin >> vertexList[3 * i] >> vertexList[3 * i + 1] >> vertexList[3 * i + 2];
+    }
+    const CornerType numberTriangles = ntriangle;
+    const CornerType numberVertices = npoint;
+    CornerType *triangleList = new CornerType[3 * ntriangle];
+    for (int i = 0; i < ntriangle; ++i)
+    {
+        fin >> var0 >> triangleList[i * 3] >> triangleList[i * 3 + 1] >> triangleList[i * 3 + 2];
+    }
+    int numberCoordinatesByVertex = 3;
+    CornerTable *CT = new CornerTable(triangleList,
+                                      vertexList,
+                                      numberTriangles,
+                                      numberVertices,
+                                      numberCoordinatesByVertex);
+    ct = CT;
+    cantidadPuntos = npoint;
+    vertexList1 = vertexList;
 }
 
-void crearCamino(int inicio, int destino){
+void crearCamino(int inicio, int destino)
+{
     vector<unsigned int> camino;
     const CornerType *trianglesPositions = ct->getTriangleList();
 
     min_path(camino, ct, inicio, destino);
 
     int aux = camino[destino];
-    indiceCamino.push_back(trianglesPositions[destino*3]);
-    indiceCamino.push_back(trianglesPositions[destino*3 + 1]);
-    indiceCamino.push_back(trianglesPositions[destino*3 + 2]);
+    indiceCamino.push_back(trianglesPositions[destino * 3]);
+    indiceCamino.push_back(trianglesPositions[destino * 3 + 1]);
+    indiceCamino.push_back(trianglesPositions[destino * 3 + 2]);
 
-while(aux>-1){
- 	indiceCamino.push_back(trianglesPositions[aux*3]);
-    	indiceCamino.push_back(trianglesPositions[aux*3 + 1]);
-    	indiceCamino.push_back(trianglesPositions[aux*3 + 2]);
+    while (aux > -1)
+    {
+        indiceCamino.push_back(trianglesPositions[aux * 3]);
+        indiceCamino.push_back(trianglesPositions[aux * 3 + 1]);
+        indiceCamino.push_back(trianglesPositions[aux * 3 + 2]);
         aux = camino[aux];
     }
-	
-	
-	void buffers() {
-	 double* vertexPositions = ct->getAttributes();
+}
+void buffers()
+{
+    double *vertexPositions = ct->getAttributes();
     const CornerType *trianglesPositions = ct->getTriangleList();
-	 for (unsigned int i=0; i<ct->getNumberVertices()*3; i++)
+    for (unsigned int i = 0; i < ct->getNumberVertices() * 3; i++)
         vertexPositions[i] *= normalize;
 
     glGenVertexArrays(1, vao);
@@ -150,127 +156,131 @@ while(aux>-1){
 
     glGenBuffers(numVBOs, vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, ct->getNumberVertices()*3*sizeof(double), &vertexPositions[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, ct->getNumberVertices() * 3 * sizeof(double), &vertexPositions[0], GL_STATIC_DRAW);
 
     glGenBuffers(numEBOs, ebo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
-	 glBufferData(GL_ELEMENT_ARRAY_BUFFER, ct->getNumTriangles()*3*sizeof(CornerType), &trianglesPositions[0], GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ct->getNumTriangles() * 3 * sizeof(CornerType), &trianglesPositions[0], GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[1]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiceCamino.size()*3*sizeof(CornerType), &indiceCamino[0], GL_STATIC_DRAW);
-
-	}
-
-
-void init (GLFWwindow* window) {
-	// Shaders
-	renderingProgram = Utils::createShaderProgram("src/vertShader.glsl", "src/fragShader.glsl");
-// Lectura de mesh
-	leerMesh();
-		// Generacion de puntos
-	double punto1x,punto1y,punto2x,punto2y;
-	generadorPuntos(punto1x,punto1y,punto2x,punto2y);
-	int puntoCercano1,puntoCercano2;
-	//	Ubicacion de puntos
-	ubicarCoordenada(vertexList1,cantidadPuntos,punto1x,punto1y,puntoCercano1);
-	ubicarCoordenada(vertexList1,cantidadPuntos,punto2x,punto2y,puntoCercano2);
-	cout<<"[" << punto1x <<" - " <<punto1y<< "]";
-	cout<<" - Es cercano a el punto "<<puntoCercano1<<endl;
-	cout<<"[" << punto2x <<" - " <<punto2y<< "]";
-	cout<<" - Es cercano a el punto "<<puntoCercano2<<endl;
-	//	Distancia minima de puntos
-	crearCamino(puntoCercano1,puntoCercano2);
-
-	//	Mostrar ID's de triangulos recorridos
-	for(unsigned int i=0; i<indiceCamino.size(); i+=3){
-	    cout<<i/3+1<<" : "<<indiceCamino[i]<<" , "<<indiceCamino[i+1]<<" , "<<indiceCamino[i+2]<<endl;
-	 }
-	 
-	 // Declarar buffers
-	buffers();
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiceCamino.size() * 3 * sizeof(CornerType), &indiceCamino[0], GL_STATIC_DRAW);
 }
 
-void display(GLFWwindow* window, double currentTime) {
+void init(GLFWwindow *window)
+{
+    // Shaders
+    renderingProgram = Utils::createShaderProgram("src/vertShader.glsl", "src/fragShader.glsl");
+    // Lectura de mesh
+    leerMesh();
+    // Generacion de puntos
+    double punto1x, punto1y, punto2x, punto2y;
+    generadorPuntos(punto1x, punto1y, punto2x, punto2y);
+    int puntoCercano1, puntoCercano2;
+    //	Ubicacion de puntos
+    ubicarCoordenada(vertexList1, cantidadPuntos, punto1x, punto1y, puntoCercano1);
+    ubicarCoordenada(vertexList1, cantidadPuntos, punto2x, punto2y, puntoCercano2);
+    cout << "[" << punto1x << " - " << punto1y << "]";
+    cout << " - Es cercano a el punto " << puntoCercano1 << endl;
+    cout << "[" << punto2x << " - " << punto2y << "]";
+    cout << " - Es cercano a el punto " << puntoCercano2 << endl;
+    //	Distancia minima de puntos
+    crearCamino(puntoCercano1, puntoCercano2);
 
-	glClear(GL_DEPTH_BUFFER_BIT);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glUseProgram(renderingProgram);
+    //	Mostrar ID's de triangulos recorridos
+    for (unsigned int i = 0; i < indiceCamino.size(); i += 3)
+    {
+        cout << i / 3 + 1 << " : " << indiceCamino[i] << " , " << indiceCamino[i + 1] << " , " << indiceCamino[i + 2] << endl;
+    }
 
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
+    // Declarar buffers
+    buffers();
+}
 
-	vec3 color1;
-	color1 = vec3(1.0, 0.00, 0.0);
-	glUniform3fv(glGetUniformLocation(renderingProgram, "vColor"), 1, value_ptr(color1));
+void display(GLFWwindow *window, double currentTime)
+{
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
-	//Dibujo de CORNER TABLE
-	for(int i=0; i<ct->getNumTriangles() ; i++){
-		color1 = vec3(i%3-0.42, i%5-0.42, i%7-0.43);
-		if(i==0){
-			color1 = vec3(1, 0.42, 0.23);
-		}
-			glUniform3fv(glGetUniformLocation(renderingProgram, "vColor"), 1, value_ptr(color1));
-	            glDrawElements(
-	                GL_TRIANGLES,
-	                3,
-	                GL_UNSIGNED_INT,
-	                (void*)(i*3*sizeof(GL_UNSIGNED_INT))
-	            );
-	        }
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	//DIBUJO DE RECORRIDO DE TRIANGULOS
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    vec3 color1;
+    color1 = vec3(1.0, 0.00, 0.0);
+    glUniform3fv(glGetUniformLocation(renderingProgram, "vColor"), 1, value_ptr(color1));
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[0]);
+    //Dibujo de CORNER TABLE
+    for (int i = 0; i < ct->getNumTriangles(); i++)
+    {
+        color1 = vec3(i % 3 - 0.42, i % 5 - 0.42, i % 7 - 0.43);
+        if (i == 0)
+        {
+            color1 = vec3(1, 0.42, 0.23);
+        }
+        glUniform3fv(glGetUniformLocation(renderingProgram, "vColor"), 1, value_ptr(color1));
+        glDrawElements(
+            GL_TRIANGLES,
+            3,
+            GL_UNSIGNED_INT,
+            (void *)(i * 3 * sizeof(GL_UNSIGNED_INT)));
+    }
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    //DIBUJO DE RECORRIDO DE TRIANGULOS
     color1 = vec3(1, 1, 1);
     glUniform3fv(glGetUniformLocation(renderingProgram, "vColor"), 1, value_ptr(color1));
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[1]);
-	glDrawElements(
-	        GL_TRIANGLES,
-			indiceCamino.size(),
-	        GL_UNSIGNED_INT,
-	        0
-	);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	// TRAZADO DE TRIANGULOS
-	color1 = glm::vec3(0.0, 0.0, 0.0);
-	glUniform3fv(glGetUniformLocation(renderingProgram, "vColor"), 1, value_ptr(color1));
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[1]);
-    for(int i=0; i<indiceCamino.size()/3 ; i++){
-    	glDrawElements(
-                 GL_LINE_LOOP,
-                 3,
-                 GL_UNSIGNED_INT,
-                 (void*)(i*3*sizeof(GL_UNSIGNED_INT))
- 	     );
-	 }
-
+    glDrawElements(
+        GL_TRIANGLES,
+        indiceCamino.size(),
+        GL_UNSIGNED_INT,
+        0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    // TRAZADO DE TRIANGULOS
+    color1 = glm::vec3(0.0, 0.0, 0.0);
+    glUniform3fv(glGetUniformLocation(renderingProgram, "vColor"), 1, value_ptr(color1));
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo[1]);
+    for (int i = 0; i < indiceCamino.size() / 3; i++)
+    {
+        glDrawElements(
+            GL_LINE_LOOP,
+            3,
+            GL_UNSIGNED_INT,
+            (void *)(i * 3 * sizeof(GL_UNSIGNED_INT)));
+    }
 }
 
-int main(void) {
-    if (!glfwInit()) {
-    	exit(EXIT_FAILURE);
+int main(void)
+{
+    if (!glfwInit())
+    {
+        exit(EXIT_FAILURE);
     }
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Corner table", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 800, "Corner table", NULL, NULL);
     glfwMakeContextCurrent(window);
-	 if (glewInit() != GLEW_OK) {
-    	exit(EXIT_FAILURE);
+    if (glewInit() != GLEW_OK)
+    {
+        exit(EXIT_FAILURE);
     }
     glfwSwapInterval(1);
 
     init(window);
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(window))
+    {
         display(window, glfwGetTime());
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-	   glfwDestroyWindow(window);
+    glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
